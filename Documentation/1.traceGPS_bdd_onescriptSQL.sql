62d92fdbac1f5dc01624df6f9a74c0c7b1953bdc
Neell0 <ethan.divet@gmail.com>
2023-12-14 14:13:47 +0100

    Ajout script SQL Onshoot

--- Documentation/1.traceGPS_bdd_onescriptSQL.sql
+++ Documentation/1.traceGPS_bdd_onescriptSQL.sql
@@ -1,23 +1,69 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Client :  127.0.0.1
--- Générée le :  Lun 11 Décembre 2017 à 16:24
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+
+DROP DATABASE IF EXISTS `AP-SIO2-traceGPS-2324_julien-preprod`;
+CREATE DATABASE `AP-SIO2-traceGPS-2324_julien-preprod`
+  CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+DROP USER tracegps@'localhost';
+CREATE USER tracegps@'localhost' IDENTIFIED BY 'spgecart';
+GRANT ALL ON `AP-SIO2-traceGPS-2324_julien-preprod`.* TO tracegps@'localhost' ;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+USE `AP-SIO2-traceGPS-2324_julien-preprod`;
+
+DROP TABLE IF EXISTS tracegps_utilisateurs;
+CREATE TABLE IF NOT EXISTS tracegps_utilisateurs (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  pseudo varchar(30) NOT NULL,
+  mdpSha1 varchar(40) NOT NULL,
+  adrMail varchar(75) NOT NULL,
+  numTel varchar(14) DEFAULT NULL,
+  niveau int(1) NOT NULL DEFAULT '1',
+  dateCreation timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  UNIQUE KEY pseudo (pseudo)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 
---
--- Contenu de la table 'tracegps_utilisateurs'
---
+DROP TABLE IF EXISTS tracegps_autorisations;
+CREATE TABLE IF NOT EXISTS tracegps_autorisations (
+  idAutorisant int(11) NOT NULL,
+  idAutorise int(11) NOT NULL,
+  PRIMARY KEY (idAutorisant,idAutorise)
+) ENGINE=InnoDB Default charset=UTF8;
+
+
+DROP TABLE IF EXISTS tracegps_traces;
+CREATE TABLE IF NOT EXISTS tracegps_traces (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  dateDebut timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dateFin timestamp NULL DEFAULT NULL,
+  terminee tinyint(1) NOT NULL,
+  idUtilisateur int(11) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+DROP TABLE IF EXISTS tracegps_points;
+CREATE TABLE IF NOT EXISTS tracegps_points (
+  idTrace int(11) NOT NULL,
+  id int(11) NOT NULL,
+  latitude double NOT NULL,
+  longitude double NOT NULL,
+  altitude double NOT NULL,
+  dateHeure timestamp NOT NULL,
+  rythmeCardio int(3) NOT NULL,
+  PRIMARY KEY (idTrace,id)
+) ENGINE=InnoDB Default charset=UTF8;
+
+
+Alter table tracegps_autorisations add constraint fkuser1 foreign key (idAutorisant) references tracegps_utilisateurs(id) ;
+Alter table tracegps_autorisations add constraint fkuser2 foreign key (idAutorise) references tracegps_utilisateurs(id) ;
+Alter table tracegps_traces add constraint fkuser3 foreign key (idUtilisateur) references tracegps_utilisateurs(id) ;
+Alter table tracegps_points add constraint fktrace foreign key (idTrace) references tracegps_traces(id) ;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
 INSERT INTO tracegps_utilisateurs (id, pseudo, mdpSha1, adrMail, numTel, niveau) VALUES
 (1, 'admin', 'ff9fff929a1292db1c00e3142139b22ee4925177', 'delasalle.sio.eleves@gmail.com', '11.22.33.44.55', 2),
 (2, 'callisto', '13e3668bbee30b004380052b086457b014504b3e', 'delasalle.sio.eleves@gmail.com', '22.33.44.55.66', 1),
@@ -33,10 +79,6 @@ INSERT INTO tracegps_utilisateurs (id, pseudo, mdpSha1, adrMail, numTel, niveau)
 (12, 'oxygen', '13e3668bbee30b004380052b086457b014504b3e', 'delasalle.sio.eleves@gmail.com', '44.55.66.77.88', 1),
 (13, 'photon', '13e3668bbee30b004380052b086457b014504b3e', 'delasalle.sio.eleves@gmail.com', '44.55.66.77.88', 1);
 
-
---
--- Contenu de la table 'tracegps_autorisations'
---
 INSERT INTO tracegps_autorisations (idAutorisant, idAutorise) VALUES
 (2, 3),
 (2, 4),
@@ -59,10 +101,6 @@ INSERT INTO tracegps_autorisations (idAutorisant, idAutorise) VALUES
 (11, 12),
 (11, 13);
 
-
---
--- Contenu de la table 'tracegps_traces'
---
 INSERT INTO tracegps_traces (id, dateDebut, dateFin, terminee, idUtilisateur) VALUES
 (1, '2018-01-19 13:08:48', NULL, 0, 2),
 (2, '2018-01-19 13:08:48', '2018-01-19 13:11:48', 1, 2),
@@ -87,10 +125,6 @@ INSERT INTO tracegps_traces (id, dateDebut, dateFin, terminee, idUtilisateur) VA
 (21, '2018-01-19 13:08:48', NULL, 0, 12),
 (22, '2018-01-19 13:08:48', '2018-01-19 13:11:48', 1, 12);
 
-
---
--- Contenu de la table 'tracegps_points'
---
 INSERT INTO tracegps_points (idTrace, id, latitude, longitude, altitude, dateHeure, rythmeCardio) VALUES
 (1, 1, 48.2109, -1.5535, 60, '2018-01-19 13:08:48', 81),
 (1, 2, 48.2119, -1.5525, 70, '2018-01-19 13:09:08', 82),
@@ -258,8 +292,15 @@ INSERT INTO tracegps_points (idTrace, id, latitude, longitude, altitude, dateHeu
 (22, 9, 48.2189, -1.5455, 140, '2018-01-19 13:11:28', 89),
 (22, 10, 48.2199, -1.5445, 150, '2018-01-19 13:11:48', 90);
 
+CREATE OR REPLACE VIEW tracegps_vue_utilisateurs (id, pseudo, mdpSha1, adrMail, numTel, niveau, dateCreation, nbTraces, dateDerniereTrace) AS
+SELECT tracegps_utilisateurs.id, pseudo, mdpSha1, adrMail, numTel, niveau, dateCreation, count(tracegps_traces.id), max(dateDebut)
+FROM tracegps_utilisateurs left join tracegps_traces ON tracegps_utilisateurs.id = tracegps_traces.idUtilisateur
+GROUP BY id, pseudo, mdpsha1, adrmail, numtel, niveau, dateCreation
+ORDER BY pseudo ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
+CREATE OR REPLACE VIEW tracegps_vue_traces (id, dateDebut, dateFin, terminee, idUtilisateur, pseudo, nbPoints) AS
+SELECT tracegps_traces.id, dateDebut, dateFin, terminee, idUtilisateur, pseudo, count(tracegps_points.id)
+FROM (tracegps_traces INNER JOIN tracegps_utilisateurs ON tracegps_traces.idUtilisateur = tracegps_utilisateurs.id)
+LEFT JOIN tracegps_points ON tracegps_traces.id = tracegps_points.idTrace
+GROUP BY id, dateDebut, dateFin, terminee, idUtilisateur, pseudo
+ORDER BY id;
